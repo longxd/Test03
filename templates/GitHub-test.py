ed6fae6d01cb58c62f1ca4cfb6118d25be35ea37
longxd <33946624+longxd@users.noreply.github.com>
2018-06-27 16:21:23 +0800

    template title 6666

--- templates/GitHub-test.py
+++ templates/GitHub-test.py
@@ -1,8 +1,5 @@
 
 def fun():
     print('hello GitHub1')
-    print('hello GitHub2')
-    print('hello GitHub7')
-    print('hello GitHub9')
-    print('hello GitHub10')
-    print('hello GitHub11')
+    print('hello test222')
+   
