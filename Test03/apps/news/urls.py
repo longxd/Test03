ba78a1c3670852f059898496344d2fe58001ac2f
longxd <1107576036@qq.com>
2018-06-27 21:47:30 +0800

    第一次备份

---
+++ Test03/apps/news/urls.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+from django.urls import path
+from . import views
+from django.shortcuts import reverse
+
+app_name = 'news'
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
---
+++ Test03/apps/xfzauth/models.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+from django.db import models
+
+# Create your models here.
+
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, telephone, username, password, **kwargs):
+        user = self.model(telephone=telephone, username=username, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, telephone, username, password, **kwargs):
+        kwargs['is_superuser'] = False
+        return self._create_user(telephone, username, password, **kwargs)
+
+    def create_superuser(self,telephone, username, password, **kwargs):
+        kwargs['is_superuser'] = True
+        return self._create_user(telephone, username, password, **kwargs)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    telephone = models.CharField(max_length=11, unique=True)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=0)
+    gender = models.IntegerField(default=0)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'telephone'
+    REQUIRED_FIELDS = ['username']
+    EMAIL_FIELD = 'email'
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
